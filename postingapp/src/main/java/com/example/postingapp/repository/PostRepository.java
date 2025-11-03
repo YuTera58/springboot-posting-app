994861ccdf76b337b8bbea79b5bcd098e2ddec9a
YuTera58 <YuTerakoya4Study@gmail.com>
2025-11-03 17:05:33 +0900

    kadai springboot-posting-app

--- postingapp/src/main/java/com/example/postingapp/repository/PostRepository.java
+++ postingapp/src/main/java/com/example/postingapp/repository/PostRepository.java
@@ -14,6 +14,9 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 	//引数に指定したユーザーに紐づく投稿を作成日時が新しい順に取得します。
 	public List<Post> findByUserOrderByCreatedAtDesc(User user);
 	
+	//課題用に追加：引数に指定したユーザーに紐づく投稿を更新日時が古い順に取得します。
+	public List<Post> findByUserOrderByUpdatedAtAsc(User user);
+	
 	//findFirstByOrderByIdDesc()は idカラムの値で降順に並べ替え、最初の1件を取得するメソッドです。
 	//つまり、idが最も大きい投稿を取得できます。
 	public Post findFirstByOrderByIdDesc();
